9c8da2ef6184a14cf59c0fdcc6304f025ccbaf71
Alex <alex@greenbit.ro>
2016-07-14 00:02:19 +0300

    Update zoiper-install.sh

--- zoiper-install.sh
+++ zoiper-install.sh
@@ -7,8 +7,9 @@ chmod +x Zoiper_3.3_Linux_Free_64Bit.run
 ./Zoiper_3.3_Linux_Free_64Bit.run --mode unattended
 rm ~/Zoiper_3.3_Linux_Free_64Bit.run
 
+mkdir /home/user1/.Zoiper/
 wget https://raw.githubusercontent.com/atrp/dsltd/master/Config.xml -O /home/user1/.Zoiper/Config.xml
-chown user1:user1 /home/user1/.Zoiper/Config.xml
+chown -R user1:user1 /home/user1/.Zoiper/
 
 cp /usr/share/applications/zoiper.desktop /home/user1/Desktop/Zoiper.desktop
 chown user1:user1 /home/user1/Desktop/Zoiper.desktop
