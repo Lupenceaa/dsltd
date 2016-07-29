205d07d82d8590d5a0414f3bdf930a9597c2de5d
Alex <alex@greenbit.ro>
2016-07-29 16:40:58 +0300

    Update create-tiger-icon.sh

--- create-tiger-icon.sh
+++ create-tiger-icon.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
+
+# add to rc.local the following lines
+# wget https://raw.githubusercontent.com/atrp/dsltd/master/create-tiger-icon.sh
+# /bin/bash /root/create-tiger-icon.sh
+
 touch /home/user1/Desktop/tiger.desktop
 chown user1:user1 /home/user1/Desktop/tiger.desktop
 chmod 755 /home/user1/Desktop/tiger.desktop
+
 cat > /home/user1/Desktop/tiger.desktop <<- EOM
 [Desktop Entry]
 Comment=
