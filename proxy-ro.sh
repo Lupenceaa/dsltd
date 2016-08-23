1cbf6842dcd50905b76323cca3d7fe79183342dc
Alex <alex@greenbit.ro>
2016-08-23 17:38:46 +0300

    Update proxy-ro.sh

--- proxy-ro.sh
+++ proxy-ro.sh
@@ -1,6 +1,6 @@
-#!/bin/sh
-su user1 -c "gsettings set org.gnome.system.proxy mode 'manual'"
-su user1 -c "gsettings set org.gnome.system.proxy.http host '192.168.88.1'"
-su user1 -c "gsettings set org.gnome.system.proxy.http port '8082'"
-su user1 -c "gsettings set org.gnome.system.proxy.https host '192.168.88.1'"
-su user1 -c "gsettings set org.gnome.system.proxy.https port '8082'"
+!/bin/sh
+su user1 -c "dbus-launch --exit-with-session gsettings set org.gnome.system.proxy mode 'manual'"
+su user1 -c "dbus-launch --exit-with-session gsettings set org.gnome.system.proxy.http host '192.168.88.1'"
+su user1 -c "dbus-launch --exit-with-session gsettings set org.gnome.system.proxy.http port '8082'"
+su user1 -c "dbus-launch --exit-with-session gsettings set org.gnome.system.proxy.https host '192.168.88.1'"
+su user1 -c "dbus-launch --exit-with-session gsettings set org.gnome.system.proxy.https port '8082'"
